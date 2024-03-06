c3c8198959c862ba5fe522115ff0e43e78b4f268
kar98bdiw <ghazaryan.98@mail.ru>
2024-03-07 01:06:49 +0400

    setup server

--- lib/reporter_api.dart
+++ lib/reporter_api.dart
@@ -1,3 +1,21 @@
+import 'dart:io';
+
 class ReporterApi {
-  void run() {}
+  Future<void> run() async {
+    final server = await createServer()
+      ..listen(requestHandler);
+
+    print('Server started: ${server.address} port ${server.port}');
+  }
+
+  Future<HttpServer> createServer() async {
+    final address = InternetAddress.loopbackIPv4;
+    const port = 4040;
+    return await HttpServer.bind(address, port);
+  }
+
+  void requestHandler(HttpRequest request) {
+    request.response.write('object');
+    request.response.close();
+  }
 }
